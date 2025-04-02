40b84497195a0112132e70b285e3a8e9f4fce1e9
Ladányi Tamás <Tommy-Kid@users.noreply.github.com>
2025-04-02 20:07:17 +0200

    Geometric Homework

---
+++ Geometriai_alakzatok/Parallelogram.java
@@ -0,0 +1,23 @@
+package Geometriai_alakzatok;
+
+public class Parallelogram extends Shape{
+
+	public Parallelogram(int x, int y){
+		super(x,y);
+		draw();
+	}
+
+	@Override
+	public void draw() {
+		for (int i = 1; i <= x; i++) {
+			for (int j = 1; j <= i; j++) 
+			    	System.out.print(" ");
+			for (int j = 1; j <= y; j++) 
+				System.out.print("*");
+			System.out.println();
+		}
+
+	}
+		
+}
+	
